8e5fc15bb8874964416c33d2ac29631fab0e5d27
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2022-02-23 01:28:00 +0530

    initial_commit

---
+++ src/mqtt_credential.h
@@ -0,0 +1,15 @@
+#ifndef _MQTT_CREDENTIALS_H
+#define _MQTT_CREDENTIALS_H
+
+#define broker_port 1883
+
+#define WIFI_PASSWORD         ""
+#define broker_address        ""
+#define WIFI_SSID             ""
+#define mqtt_client_id        ""
+#define mqtt_client_username  ""
+#define mqtt_client_password  ""
+#define mqtt_subscribe_topic  ""
+#define mqtt_publish_topic    ""
+
+#endif //_MQTT_CREDENTIALS_H`
